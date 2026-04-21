f91fdabb87f8c2d72c54c381ec134983b05e29e9
weathon <wguo6358@gmail.com>
2026-04-21 16:46:21 -0700

    run.sh: Tue 21 Apr 2026 04:46:21 PM PDT

--- new/run.sh
+++ new/run.sh
@@ -1,15 +1,13 @@
-export OPENAI_DEFAULT_MODEL="glm-5.1"
-export HARSH_MODEL="glm-5.1"
-export MERGER_MODEL="glm-5.1"
-export NEUTRAL_MODEL="glm-5.1"
-export OUTPUT_CSV="results/bench_scores_glm_or.csv"
-export MERGE_LOG="results/pipeline_whole_glm_or.log"
-export CONCURRENCY=10
+export OPENAI_DEFAULT_MODEL="gpt-5.1"
+export HARSH_MODEL="gpt-5.1" 
+export MERGER_MODEL="gpt-5.1"
+export NEUTRAL_MODEL="gpt-5.1"
+export OUTPUT_CSV="results/bench_scores_gpt_or.csv"
+export MERGE_LOG="results/pipeline_whole_gpt_or.log"
+export SUBAGENT_MODEL="gpt-5.1-mini"
+
 # rm bench_scores_qwen.log
 ollama serve & 
 git commit -am "run.sh: $(date)"
-# python main.py --n_samples 300 --benchmark ../iclr2025/ --seed $(cksum <<< '🍍' | cut -f 1 -d ' ') 
-python main.py --n_samples 100 --benchmark ../iclr2025/ --seed $(cksum <<< 'who 3r324f34 you' | cut -f 1 -d ' ') 
-# python main.py --n_samples 200 --benchmark ../iclr2025/ --seed $(cksum <<< 'asfwekhjfwefw' | cut -f 1 -d ' ')
-# python main.py --n_samples 500 --benchmark ../iclr2026_cspaper/ --seed $(cksum <<< 'asfwekhjfwefw' | cut -f 1 -d ' ') 
-# python main.py --n_samples 500 --benchmark ../iclr2025/ --seed $(cksum <<< 'asfwekhjfwefw' | cut -f 1 -d ' ')
+python main.py --n_samples 300 --benchmark ../iclr2025/ --seed $(cksum <<< '🍍' | cut -f 1 -d ' ') 
+# python main.py --n_samples 100 --benchmark ../iclr2025/ --seed $(cksum <<< 'who 3r324f34 you' | cut -f 1 -d ' ') 
