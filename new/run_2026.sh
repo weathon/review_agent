ab812e1c8c2c5005256013d6802ecad6ad566901
weathon <wguo6358@gmail.com>
2026-04-21 23:44:43 -0700

    run.sh: Tue 21 Apr 2026 11:44:43 PM PDT test 2026

--- new/run_2026.sh
+++ new/run_2026.sh
@@ -6,8 +6,7 @@ export OUTPUT_CSV="results/bench_scores_glm_2026.csv"
 export MERGE_LOG="results/pipeline_whole_glm_2026.log"
 export SUBAGENT_MODEL="ollama:glm-5.1:cloud"
 export CONCURRENCY=2
-# rm bench_scores_qwen.log
 ollama serve & 
 git add .
 git commit -m "run.sh: $(date) test 2026"
-python main.py --n_samples 300 --benchmark ../iclr2026_unbalanced/ --seed $(cksum <<< '45678765456787654567' | cut -f 1 -d ' ') 
+python main.py --n_samples 300 --benchmark ../iclr2026_cspaper_new/ --seed $(cksum <<< '45678765456787654567' | cut -f 1 -d ' ') 
