93e0d5387839616636bba237c8c8f89f61794efb
weathon <wguo6358@gmail.com>
2026-04-21 21:00:58 -0700

    run.sh: Tue 21 Apr 2026 09:00:58 PM PDT back to test 2025 benchmark using ollama

--- new/run.sh
+++ new/run.sh
@@ -1,13 +1,13 @@
-export OPENAI_DEFAULT_MODEL="glm-5.1"
-export HARSH_MODEL="glm-5.1" 
-export MERGER_MODEL="glm-5.1"
-export NEUTRAL_MODEL="glm-5.1"
+export OPENAI_DEFAULT_MODEL="ollama:glm-5.1:cloud"
+export HARSH_MODEL="ollama:glm-5.1:cloud" 
+export MERGER_MODEL="ollama:glm-5.1:cloud"
+export NEUTRAL_MODEL="ollama:glm-5.1:cloud"
 export OUTPUT_CSV="results/bench_scores_glm_2026_or.csv"
 export MERGE_LOG="results/pipeline_whole_glm_2026_or.log"
-export SUBAGENT_MODEL="glm-5.1"
-export CONCURRENCY=10
+export SUBAGENT_MODEL="ollama:glm-5.1:cloud"
+export CONCURRENCY=5
 # rm bench_scores_qwen.log
 ollama serve & 
-git commit -am "run.sh: $(date)"
+git commit -am "run.sh: $(date) back to test 2025 benchmark using ollama"
 # python main.py --n_samples 300 --benchmark ../iclr2026_cspaper/ --seed $(cksum <<< '🍍' | cut -f 1 -d ' ') 
-python main.py --n_samples 100 --benchmark ../iclr2025/ --seed $(cksum <<< '23456789876543234567' | cut -f 1 -d ' ') 
+python main.py --n_samples 800 --benchmark ../iclr2025/ --seed $(cksum <<< '23456789876543234567' | cut -f 1 -d ' ') 
