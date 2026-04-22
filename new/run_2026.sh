0d4535b81c4da6353d2ff09f96b321cef1fe3fb6
weathon <wguo6358@gmail.com>
2026-04-21 22:19:23 -0700

    run.sh: Tue 21 Apr 2026 10:19:23 PM PDT test 2026

--- new/run_2026.sh
+++ new/run_2026.sh
@@ -2,8 +2,8 @@ export OPENAI_DEFAULT_MODEL="glm-5.1"
 export HARSH_MODEL="glm-5.1" 
 export MERGER_MODEL="glm-5.1"
 export NEUTRAL_MODEL="glm-5.1"
-export OUTPUT_CSV="results/bench_scores_glm.csv"
-export MERGE_LOG="results/pipeline_whole_glm.log"
+export OUTPUT_CSV="results/bench_scores_glm_2026.csv"
+export MERGE_LOG="results/pipeline_whole_glm_2026.log"
 export SUBAGENT_MODEL="glm-5.1"
 export CONCURRENCY=5
 # rm bench_scores_qwen.log
