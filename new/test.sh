350634e1a65f152d7b4d0d65b8e5e020d8d5027d
weathon <wguo6358@gmail.com>
2026-04-22 23:45:33 -0700

    run.sh: Wed 22 Apr 2026 11:45:33 PM PDT do not change anymore, wait till final

--- new/test.sh
+++ new/test.sh
@@ -8,12 +8,12 @@
 # export HARSH_MODEL="ollama:kimi-k2.5:cloud"
 # export MERGER_MODEL="ollama:kimi-k2.5:cloud"
 # export NEUTRAL_MODEL="ollama:kimi-k2.5:cloud"
-
+export ANTHROPIC_API_KEY=""
 export OPENAI_DEFAULT_MODEL="z-ai/glm-5.1"
-export HARSH_MODEL="glm-5.1"
-export MERGER_MODEL="glm-5.1"
+export HARSH_MODEL="claude_sdk:claude-sonnet-4-6"
+export MERGER_MODEL="claude_sdk:claude-sonnet-4-6"
 export NEUTRAL_MODEL="glm-5.1"
 
 ollama serve &
-python main.py --single_paper ../paper.md --accept_csv results/bench_scores_kimi.csv
+python main.py --single_paper ./papers/2601.22638v1.pdf # --accept_csv results/bench_scores_kimi.csv
 # python main.py --single_paper ./papers/paper.md
