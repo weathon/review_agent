33f751148afd3b25d7b13e07ff290d67c4d52972
weathon <wguo6358@gmail.com>
2026-04-22 18:13:22 -0700

    run.sh: Wed 22 Apr 2026 06:13:22 PM PDT do not change anymore, wait till final

--- new/run.sh
+++ new/run.sh
@@ -1,14 +1,14 @@
-export OPENAI_DEFAULT_MODEL="gpt-5.1"
-export HARSH_MODEL="gpt-5.1" 
-export MERGER_MODEL="gpt-5.1"
-export NEUTRAL_MODEL="gpt-5.1"
+export OPENAI_DEFAULT_MODEL="glm-5.1"
+export HARSH_MODEL="glm-5.1" 
+export MERGER_MODEL="glm-5.1"
+export NEUTRAL_MODEL="glm-5.1"
 export CALIBRATION_SET="2025"
-export OUTPUT_CSV="results/bench_scores_gpt-5.1_2025.csv"
-export MERGE_LOG="results/pipeline_whole_gpt-5.1_2025.log"
-export SUBAGENT_MODEL="gpt-5.1"
-export CONCURRENCY=20
+export OUTPUT_CSV="results/bench_scores_glm-5.1_2025.csv"
+export MERGE_LOG="results/pipeline_whole_glm-5.1_2025.log"
+export SUBAGENT_MODEL="glm-5.1"
+export CONCURRENCY=10
 # rm bench_scores_qwen.log
 ollama serve & 
-git commit -am "run.sh: $(date)"
+git commit -am "run.sh: $(date) do not change anymore, wait till final"
 # python main.py --n_samples 300 --benchmark ../iclr2026_cspaper/ --seed $(cksum <<< '🍍' | cut -f 1 -d ' ') 
 python main.py --n_samples 300 --benchmark ../iclr2025/ --seed $(cksum <<< '23456789876543234567' | cut -f 1 -d ' ') 
