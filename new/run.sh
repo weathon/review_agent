27ce560c5d5f0f0364b7ecaf4a530bab55100d89
weathon <wguo6358@gmail.com>
2026-04-24 05:12:46 -0700

    run.sh: Fri 24 Apr 2026 05:12:46 AM PDT do not change anymore, wait till final

--- new/run.sh
+++ new/run.sh
@@ -1,11 +1,11 @@
 export OPENAI_DEFAULT_MODEL="glm-5.1"
-export HARSH_MODEL="deepseek/deepseek-v4-flash" 
-export MERGER_MODEL="deepseek/deepseek-v4-flash"
-export NEUTRAL_MODEL="deepseek/deepseek-v4-flash"
+export HARSH_MODEL="deepseek-v4-flash" 
+export MERGER_MODEL="deepseek-v4-flash"
+export NEUTRAL_MODEL="deepseek-v4-flash"
 export CALIBRATION_SET="2025"
 export OUTPUT_CSV="results/bench_scores_ds_2025.csv"
 export MERGE_LOG="results/pipeline_whole_ds_2025.log"
-export SUBAGENT_MODEL="deepseek/deepseek-v4-flash"
+export SUBAGENT_MODEL="deepseek-v4-flash"
 export CONCURRENCY=10
 # rm bench_scores_qwen.log
 ollama serve & 
