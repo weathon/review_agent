f8f7e17e539f298132cc3b76ebf6300bb5d91368
weathon <wguo6358@gmail.com>
2026-04-27 01:06:46 -0700

    run.sh: Mon 27 Apr 2026 01:06:46 AM PDT

--- new/run_claude.sh
+++ new/run_claude.sh
@@ -22,13 +22,13 @@ export OPENAI_DEFAULT_MODEL="z-ai/glm-5.1"
 export HARSH_MODEL="claude_sdk:claude-sonnet-4-6"
 export MERGER_MODEL="claude_sdk:claude-sonnet-4-6"
 export NEUTRAL_MODEL="ollama:glm-5.1:cloud"
-export OUTPUT_CSV="bench_scores_claude_sonnet_2026.csv"
-export CALIBRATION_SET="2026"
+export OUTPUT_CSV="bench_scores_claude_sonnet_2025.csv"
+export CALIBRATION_SET="2025"
 export MERGE_LOG="pipeline_whole_claude_3.log"
 # export MAX_PAPERS=100
 ollama serve & 
 # use balanced to see quality in the whole spectrum of scores
 export CONCURRENCY=1
 export MAX_PAPERS=50
-python main.py --n_samples 200 --benchmark ../iclr2026_cspaper_new/ --seed $(cksum <<< '3456789' | cut -f 1 -d ' ') 
-# python main.py --n_samples 200 --benchmark ../iclr2025/ --seed $(cksum <<< '45678654567875456' | cut -f 1 -d ' ') 
+# python main.py --n_samples 200 --benchmark ../iclr2026_cspaper_new/ --seed $(cksum <<< '3456789' | cut -f 1 -d ' ') 
+python main.py --n_samples 200 --benchmark ../iclr2025/ --seed $(cksum <<< '45678654567875456' | cut -f 1 -d ' ') 
